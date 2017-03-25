d5b6b3e103fa6af1a12480053edaf1760b60a5bc
Daniel E. Renfer <duck@kronkltd.net>
2017-03-25 16:24:03 -0400

    protractor

--- protractor.config.js
+++ protractor.config.js
@@ -2,13 +2,13 @@ var config = require('cukefarm').config;
 
 config.specs = 'features/*.feature';
 config.baseUrl = 'http://jiksnu-integration.docker/';
-config.capabilities.browserName = 'chrome';
-// config.capabilities.browserName = 'phantomjs';
+// config.capabilities.browserName = 'chrome';
+config.capabilities.browserName = 'phantomjs';
 config.cucumberOpts.format = "pretty";
 // config.cucumberOpts.format = "summary";
 config.cucumberOpts.require.push('target/protractor-tests.js');
 
-config.seleniumAddress = 'http://webdriver:24444/wd/hub';
+// config.seleniumAddress = 'http://webdriver:24444/wd/hub';
 
 config.plugins = [];
 // config.plugins.push(
