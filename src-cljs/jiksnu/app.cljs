556de9433b5d3098d8d1e21e4c42dd04c783990d
Daniel E. Renfer <duck@kronkltd.net>
2017-06-27 20:30:25 -0400

    move factories

--- src-cljs/jiksnu/app.cljs
+++ src-cljs/jiksnu/app.cljs
@@ -2,34 +2,31 @@
   (:require [jiksnu.model :as model]
             [jiksnu.registry :as registry]
             [jiksnu.app.loader :as loader]
-            [jiksnu.app.providers :as providers]
             [taoensso.timbre :as timbre]))
 
 (defonce models  (atom {}))
 
 (defonce jiksnu (loader/initialize-module! plugins))
 
-(.provider jiksnu "app" providers/app)
-
 (-> jiksnu
-    (.factory "Activities"       #js ["DS" Activities])
-    (.factory "Albums"           #js ["DS" Albums])
-    (.factory "Clients"          #js ["DS" Clients])
-    (.factory "Conversations"    #js ["DS" "subpageService" Conversations])
-    (.factory "Domains"          #js ["DS" Domains])
-    (.factory "FeedSources"      #js ["DS" FeedSources])
-    (.factory "Followings"       #js ["DS" Followings])
-    (.factory "Groups"           #js ["DS" Groups])
-    (.factory "GroupMemberships" #js ["DS" GroupMemberships])
-    (.factory "Likes"            #js ["DS" Likes])
-    (.factory "Notifications"    #js ["DS" Notifications])
-    (.factory "Pages"            #js ["DS" Pages])
-    (.factory "Pictures"         #js ["DS" Pictures])
-    (.factory "RequestTokens"    #js ["DS" RequestTokens])
-    (.factory "Resources"        #js ["DS" Resources])
-    (.factory "Services"         #js ["DS" Services])
-    (.factory "Streams"          #js ["DS" Streams])
-    (.factory "Subscriptions"    #js ["DS" Subscriptions])
-    (.factory "Users"            #js ["DS" "subpageService" Users]))
+    (.factory "Activities"       #js ["DS" model/Activities])
+    (.factory "Albums"           #js ["DS" model/Albums])
+    (.factory "Clients"          #js ["DS" model/Clients])
+    (.factory "Conversations"    #js ["DS" "subpageService" model/Conversations])
+    (.factory "Domains"          #js ["DS" model/Domains])
+    (.factory "FeedSources"      #js ["DS" model/FeedSources])
+    (.factory "Followings"       #js ["DS" model/Followings])
+    (.factory "Groups"           #js ["DS" model/Groups])
+    (.factory "GroupMemberships" #js ["DS" model/GroupMemberships])
+    (.factory "Likes"            #js ["DS" model/Likes])
+    (.factory "Notifications"    #js ["DS" model/Notifications])
+    (.factory "Pages"            #js ["DS" model/Pages])
+    (.factory "Pictures"         #js ["DS" model/Pictures])
+    (.factory "RequestTokens"    #js ["DS" model/RequestTokens])
+    (.factory "Resources"        #js ["DS" model/Resources])
+    (.factory "Services"         #js ["DS" model/Services])
+    (.factory "Streams"          #js ["DS" model/Streams])
+    (.factory "Subscriptions"    #js ["DS" model/Subscriptions])
+    (.factory "Users"            #js ["DS" "subpageService" model/Users]))
 
 (.controller jiksnu "AppController" #js [(fn [])])
